235c602679ff3e3b342518e80822294d44ffe0f1
behlah <behlah.zainab@gmail.com>
2019-12-24 23:27:52 -0500

    i got a string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
+         int behlah = 1;
+         System.out.println(behlah);
+         String hello = "i have made a variable";
+         System.out.println(hello);
     }
 }
