eed426e59c7ae6526644818ce9b784e6a9e494d1
behlah <behlah.zainab@gmail.com>
2019-12-27 19:02:36 -0500

    double time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-         int behlah = 1;
+         double mamaaaooohh = 4.0 + 5.0;
+         double behlah = 4.0 - 5.0;
+         double yeet = 4.0 * 5.0;
+         double babeet = 4.0 / 5.0;
+         System.out.println(mamaaaooohh);
          System.out.println(behlah);
-         String hello = "i have made a variable";
-         System.out.println(hello);
-         int yeet = 1 + 1;
-         int babeet = 1 - 1;
-         int bayeet = 1 * 1;
-         int mamaaaoooh = 1 / 1;
          System.out.println(yeet);
          System.out.println(babeet);
-         System.out.println(bayeet);
-         System.out.println(mamaaaoooh);
     }
 }
